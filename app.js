5085e34369d096f6a4ad9a849efde51e61dab981
willwos <103256644+willwos@users.noreply.github.com>
2022-11-30 12:25:29 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Grab the day of the week from local computer
+// const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// let current = 0;
+
+/* function weekday(){
+    document.getElementById('weekday').innerHTML = days[current++ % days.length];
+} */
+
+// This wasn't needed in the end - the JS ran through the index.html file. Before I could figure out how to get the current time on the button click, I had the button rotating through Monday - Sunday at random.
